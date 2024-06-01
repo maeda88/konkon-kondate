466f78be4faa481545b1e4ba8aadfe1367366e56
maeda88 <147219013+maeda88@users.noreply.github.com>
2024-06-02 08:57:51 +0900

    categoryの定義、アソシエーションの記述

---
+++ app/models/category.rb
@@ -0,0 +1,15 @@
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '炒める・焼き' },
+    { id: 3, name: '煮物' },
+    { id: 5, name: '蒸す・ゆでる' },
+    { id: 6, name: '揚げ物' },
+    { id: 4, name: '生' },
+    { id: 7, name: 'その他' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :dishes
+
+  end
