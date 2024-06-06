de74da9397fa0472e5b026182a266953d59be979
maeda88 <147219013+maeda88@users.noreply.github.com>
2024-06-06 21:44:08 +0900

    カテゴリーの選択　修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,9 +3,9 @@ class Category < ActiveHash::Base
     { id: 1, name: '---' },
     { id: 2, name: '炒める・焼き' },
     { id: 3, name: '煮物' },
-    { id: 5, name: '蒸す・ゆでる' },
-    { id: 6, name: '揚げ物' },
-    { id: 4, name: '生' },
+    { id: 4, name: '蒸す・ゆでる' },
+    { id: 5, name: '揚げ物' },
+    { id: 6, name: '生' },
     { id: 7, name: 'その他' }
   ]
 
