ee84c6c257fb75e0927abbade266864ebdd8c8bb
maeda88 <147219013+maeda88@users.noreply.github.com>
2024-06-10 15:37:16 +0900

    menuesコントローラー作成

---
+++ spec/helpers/menues_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the MenuesHelper. For example:
+#
+# describe MenuesHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe MenuesHelper, type: :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
---
+++ spec/requests/menues_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Menues", type: :request do
+
+end
