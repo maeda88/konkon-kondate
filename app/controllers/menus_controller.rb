c6be6505cf0de1072f77e8a3e863122c7b60e4a4
maeda88 <147219013+maeda88@users.noreply.github.com>
2024-07-01 22:33:36 +0900

    コントローラーの記述

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -3,4 +3,24 @@ class MenusController < ApplicationController
     @menus = Menu.all
     @menu = Menu.new
   end
+
+  def new
+    @menu = Menu.new
+  end
+
+  def create
+    binding.pry
+    @menu = Menu.create(menu_params)
+    # if @menu.save
+      redirect_to root_path
+    # else
+      # render :new, status: :unprocessable_entity
+    # end
+  end
+  
+  private
+
+  def menu_params
+    params.require(:menu).permit(:start_day).merge()
+  end
 end
